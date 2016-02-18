121445733e0405475c5721f9b9da793ab03be3a4
Anton Qiu <qiuchangtong@gmail.com>
2016-02-18 09:08:06 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
 	initializePage();
 });
 
+$(".likeBtn").click(function(e) {
+    console.log("like clicked");
+    ga('send', 'event', 'like', 'click');
+})
+
 /*
  * Function that is called when the document is ready.
  */
