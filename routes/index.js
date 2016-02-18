fb9f7e3f91bb504022a12723b673ff82851b08fd
Anton Qiu <qiuchangtong@gmail.com>
2016-02-18 09:23:44 +0000

    exp

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,21 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function (req, res) {
     projects["grid"] = false;
-    res.render('index', projects);
+    var random_num = Math.random();
+    console.log(random_num);
+
+    if (random_num >= 0) {
+        projects['grid'] = false;
+        res.render('index', projects);
+    } else {
+        res.redirect('/grid');
+    }
+
 };
 
-exports.viewGrid = function(req, res){
+exports.viewGrid = function (req, res) {
     projects["grid"] = true;
-    res.render('index', projects);  
+    res.render('index', projects);
 };
